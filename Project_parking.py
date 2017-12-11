5360361dcd4a51a8a0e2a059cdee65b293f202ca
rreksha <32369437+rreksha@users.noreply.github.com>
2017-12-10 23:22:22 -0800

    Update Project_parking.py

--- Project_parking.py
+++ Project_parking.py
@@ -18,11 +18,55 @@ def safeGet(url):
         print("Reason: ", e.reason)
     return None
 
-def open():
+def openParking():
     url = "https://data.seattle.gov/resource/kdkm-y3uh.json"
     urlOpen = safeGet(url)
     data = json.loads(urlOpen.read())
     return data
 
-data = open()
-print(pretty(data))
+
+def location(lat,long):
+    data = openParking()
+    list = []
+    for lot in data:
+        latitude = lot["shape"]["latitude"]
+        longitude = lot["shape"]["longitude"]
+        if((lat+0.0724637)>= float(latitude) and (lat-0.0724637)<= float(latitude)):
+            if((long +0.0724637)>= float(longitude) and (long -0.0724637)<= float(longitude)):
+                list.append(lot)
+    return list
+
+class ParkingInfo:
+    def __init__(self,data):
+        if (data.get("fac_type") == None):
+            self.type = 'Unknown'
+        else:
+            self.type = data['fac_type']
+        if (data.get("disbaled") == None):
+            self.disabled = 'Unknown'
+        else:
+            self.disabled = data['disabled']
+        if (data.get("rte_1hr") == None):
+            self.rate1hour = 'Unknown'
+        else:
+            self.rate1hour = data['rte_1hr']
+        if (data.get("rte_allday") == None):
+            self.rateAllDay = 'Unknown'
+        else:
+            self.rateAllDay = data['rte_allday']
+
+
+# Testing
+# data = openParking()
+# print(pretty(data))
+# list = location(47.6,-122.3)
+# for dic in list:
+#     info = ParkingInfo(dic)
+#     print(info.rateAllDay)
+
+
+
+
+
+
+
